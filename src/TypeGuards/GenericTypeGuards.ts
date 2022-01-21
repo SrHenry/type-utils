9ff6e2599225a1d3a215dd53442f4fe86b56ed2e
SrHenry <lucapvh949@gmail.com>
2022-01-21 09:26:37 -0400

    .

--- src/TypeGuards/GenericTypeGuards.ts
+++ src/TypeGuards/GenericTypeGuards.ts
@@ -1,7 +1,6 @@
 import { GetOptional } from '../types/GetOptional'
 import { GetRequired } from '../types/GetRequired'
 import { TypeGuardError } from './TypeErrors'
-import util from 'util'
 
 export type ConstructorSignature<T = any> = new (...args: any[]) => T
 export type TypeGuard<T = any> = (value: unknown) => value is T
@@ -9,7 +8,9 @@ export type GetTypeGuard<T> = T extends TypeGuard<infer U> ? U : never
 
 export type ResolveIfTypeGuard<T> = T extends TypeGuard<infer U> ? U : T
 
-export type GetStringKeys<T> = { [K in keyof T]: K extends string ? K : never }[keyof T]
+export type GetStringKeys<T> = {
+    [K in keyof T]: K extends string ? K : never
+}[keyof T]
 
 export type RequiredKeys<T> = (keyof GetRequired<T>)[]
 export type OptionalKeys<T> = (keyof GetOptional<T>)[]
@@ -33,30 +34,19 @@ export function ensureInterface<Interface, Instance = unknown>(
     value: Instance,
     validator: ((value: unknown) => boolean) & { __message__?: string }
 ): Interface {
-    if (!(validator as TypeGuard<Interface>)(value))
-    {
-        let message = `Failed while ensuring interface type constraint of ${JSON.stringify(value)} against ${JSON.stringify(validator)}`
+    if (!(validator as TypeGuard<Interface>)(value)) {
+        let message = `Failed while ensuring interface type constraint of ${JSON.stringify(
+            value
+        )} against ${JSON.stringify(validator)}`
         if ('__message__' in validator)
-            message = `Failed while ensuring interface type constraint of ${JSON.stringify(value)} against ${JSON.stringify(validator['__message__'])}`
+            message = `Failed while ensuring interface type constraint of ${JSON.stringify(
+                value
+            )} against ${JSON.stringify(validator['__message__'])}`
 
-        throw new TypeGuardError(
-            message,
-            value,
-            validator
-        )
+        throw new TypeGuardError(message, value, validator)
     }
 
     return value
-}
-export declare namespace ensureInterface {
-    function __promisify__<Interface, Instance = unknown>(
-        value: Instance,
-        validator: TypeGuard<Interface>
-    ): Promise<Interface>
-    function __promisify__<Interface, Instance = unknown>(
-        value: Instance,
-        validator: (value: unknown) => boolean
-    ): Promise<Interface>
 }
 
 export function is<Interface>(value: unknown, validator: TypeGuard<Interface>): value is Interface
@@ -80,14 +70,8 @@ export function ensureInstanceOf<Instance, Constructor extends ConstructorSignat
         throw new TypeGuardError(`Value is not an instance of ${type.name}`, value, type)
     return value
 }
-export declare namespace ensureInstanceOf {
-    function __promisify__<Instance, Constructor extends ConstructorSignature>(
-        value: Instance,
-        type: Constructor
-    ): Promise<InstanceType<Constructor>>
-}
 
-export namespace Promisify {
+namespace Promisify {
     export async function ensureInterface<Interface, Instance = unknown>(
         value: Instance,
         validator: TypeGuard<Interface>
@@ -121,20 +105,19 @@ export namespace Promisify {
     }
 }
 
-ensureInterface[util.promisify.custom] = Promisify.ensureInterface
-ensureInstanceOf[util.promisify.custom] = Promisify.ensureInstanceOf
-
-export const ensureInterfaceAsync = util.promisify(ensureInterface)
-export const ensureInstanceOfAsync = util.promisify(ensureInstanceOf)
+export const ensureInterfaceAsync = Promisify.ensureInterface
+export const ensureInstanceOfAsync = Promisify.ensureInstanceOf
 
 export const imprintMetadata = <T, U>(key: string | symbol, metadata: T, arg: U): U => {
     return Object.assign(arg, { [key]: metadata })
 }
-export const retrieveMetadata = <T, U extends string | symbol, V extends TypeGuard>(key: U, arg: T, metadataSchema?: V): GetTypeGuard<V> | undefined => {
+export const retrieveMetadata = <T, U extends string | symbol, V extends TypeGuard>(
+    key: U,
+    arg: T,
+    metadataSchema?: V
+): GetTypeGuard<V> | undefined => {
     const hasMetadata = (arg: any): arg is { [K in U]: GetTypeGuard<V> } =>
-        arg &&
-        key in arg &&
-        (metadataSchema?.(arg[key]) ?? true)
+        arg && key in arg && (metadataSchema?.(arg[key]) ?? true)
 
     try {
         const { [key]: __metadata__ } = ensureInterface(arg, hasMetadata)
@@ -149,9 +132,7 @@ export const imprintMessage = <T>(message: string, arg: T): T => {
 }
 export const retrieveMessage = <T>(arg: T): string => {
     const hasMessage = (arg: any): arg is { __message__?: string } =>
-        arg &&
-        '__message__' in arg &&
-        typeof arg['__message__'] === 'string'
+        arg && '__message__' in arg && typeof arg['__message__'] === 'string'
 
     try {
         const { __message__ } = ensureInterface(arg, hasMessage)
@@ -165,10 +146,10 @@ export const imprintMessageFormator = <T>(formator: (...args: any[]) => string, 
     return Object.assign(arg, { __message_formator__: formator })
 }
 export const retrieveMessageFormator = <T>(arg: T) => {
-    const hasMessageFormator = (arg: any): arg is { __message_formator__?(...args: any[]): string } =>
-        arg &&
-        '__message_formator__' in arg &&
-        typeof arg['__message_formator__'] === 'function'
+    const hasMessageFormator = (
+        arg: any
+    ): arg is { __message_formator__?(...args: any[]): string } =>
+        arg && '__message_formator__' in arg && typeof arg['__message_formator__'] === 'function'
 
     try {
         const { __message_formator__ } = ensureInterface(arg, hasMessageFormator)
