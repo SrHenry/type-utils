2a6ee2fa7cc66a697e3a02c55f554797947cef6c
SrHenry <lucapvh949@gmail.com>
2025-07-22 12:05:23 -0400

    fix: :hotfix: add missing string rules exports

--- src/rules/string/index.ts
+++ src/rules/string/index.ts
@@ -2,4 +2,4 @@ import { StringRules } from '../../validators/rules'
 
 export { StringRules }
 
-export const { max, min, nonEmpty, optional, regex } = StringRules
+export const { max, min, nonEmpty, optional, regex, email, url } = StringRules
