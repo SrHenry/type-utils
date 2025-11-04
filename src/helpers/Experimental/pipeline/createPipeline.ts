b6d821ce670b901ae32335b23acbd49c6c0d594d
SrHenry <lucapvh949@gmail.com>
2025-11-04 12:37:42 -0400

    feat: :sparkles: redefine `createPipeline` function with overloads for enhanced type support

--- src/helpers/Experimental/pipeline/createPipeline.ts
+++ src/helpers/Experimental/pipeline/createPipeline.ts
@@ -1,3 +1,15 @@
+import type { Func1 } from '../../../types/Func'
+import type { GetPipeline } from './types'
+import type { internal } from './types/Pipable'
+
 import { pipeline } from './helpers/pipeline'
 
-export const createPipeline = () => pipeline()
+export function createPipeline(): internal.HasPipe<void>
+export function createPipeline<T extends Func1<any, any>>(cb: T): GetPipeline<typeof cb>
+
+export function createPipeline(this: unknown, ...args: [Func1<any, any>?, ...any]) {
+    if (args.length === 0) return pipeline()
+
+    const [cb] = args
+    return pipeline((o: Func1<unknown, unknown>) => o)(cb!)
+}
