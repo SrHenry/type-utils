630985971ae5240d13c7d48e14a0dd9ce4073d61
SrHenry <lucapvh949@gmail.com>
2025-11-06 17:17:38 -0400

    fix: :bug: `$switch` experimental feature types
    Fixes types mismatch in edge cases

--- src/types/Predicate.ts
+++ src/types/Predicate.ts
@@ -1,3 +1,4 @@
-import type { Func } from './Func'
-
-export type Predicate<T> = Func<[value: T], boolean>
+// export type Predicate<T> = Func<[value: T], boolean>
+export type Predicate<T, U extends T = T> = T extends U
+    ? (value: T) => boolean
+    : (value: T) => value is U
