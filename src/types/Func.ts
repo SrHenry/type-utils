c68d19c991ca991059c3ed853b537edfe17cda46
SrHenry <lucapvh949@gmail.com>
2025-11-06 16:18:11 -0400

    feat: :sparkles: add new function types and async variants to enhance type utility
    Adds comprehensive function and async type utilities
    
    Introduces new generic and arity-based function type definitions, including async variants, to improve type expressiveness and utility. Enhances reusability and consistency for function signatures across the codebase.

--- src/types/Func.ts
+++ src/types/Func.ts
@@ -1,3 +1,5 @@
+import { TupleTools } from './Tuple'
+
 export type NoParamsFunc<ReturnType = void> = Func<[], ReturnType>
 export type Func0<ReturnType = void> = NoParamsFunc<ReturnType>
 
@@ -124,6 +126,179 @@ export type AsyncFunc<Params extends any[] = [], ReturnType = void> = Func<
 >
 
 export type Factory<Args extends any[], ReturnType> = Func<Args, ReturnType>
+
+export type Fn<Params extends number | any[] = 0, ReturnType = void> = Params extends number
+    ? Fn<TupleTools.CreateTuple<Params>, ReturnType>
+    : Params extends [...any]
+    ? (...args: Params) => ReturnType
+    : never
+
+export type AsyncFn<Params extends number | any[] = 0, ReturnType = void> = Params extends number
+    ? AsyncFn<TupleTools.CreateTuple<Params>, ReturnType>
+    : Params extends [...any]
+    ? (...args: Params) => Promise<ReturnType>
+    : never
+
+export type Fn0<ReturnType = void> = () => ReturnType
+export type AsyncFn0<ReturnType = void> = () => Promise<ReturnType>
+export type Fn1<Param1 = any, ReturnType = void> = Fn<[Param1], ReturnType>
+export type AsyncFn1<Param1 = any, ReturnType = void> = AsyncFn<[Param1], ReturnType>
+export type Fn2<Param1 = any, Param2 = any, ReturnType = void> = Fn<[Param1, Param2], ReturnType>
+export type AsyncFn2<Param1 = any, Param2 = any, ReturnType = void> = AsyncFn<
+    [Param1, Param2],
+    ReturnType
+>
+export type Fn3<Param1 = any, Param2 = any, Param3 = any, ReturnType = void> = Fn<
+    [Param1, Param2, Param3],
+    ReturnType
+>
+export type AsyncFn3<Param1 = any, Param2 = any, Param3 = any, ReturnType = void> = AsyncFn<
+    [Param1, Param2, Param3],
+    ReturnType
+>
+export type Fn4<Param1 = any, Param2 = any, Param3 = any, Param4 = any, ReturnType = void> = Fn<
+    [Param1, Param2, Param3, Param4],
+    ReturnType
+>
+export type AsyncFn4<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4], ReturnType>
+export type Fn5<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    ReturnType = void
+> = Fn<[Param1, Param2, Param3, Param4, Param5], ReturnType>
+export type AsyncFn5<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4, Param5], ReturnType>
+export type Fn6<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    ReturnType = void
+> = Fn<[Param1, Param2, Param3, Param4, Param5, Param6], ReturnType>
+export type AsyncFn6<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4, Param5, Param6], ReturnType>
+export type Fn7<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    ReturnType = void
+> = Fn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7], ReturnType>
+export type AsyncFn7<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7], ReturnType>
+export type Fn8<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    ReturnType = void
+> = Fn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8], ReturnType>
+export type AsyncFn8<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8], ReturnType>
+export type Fn9<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    Param9 = any,
+    ReturnType = void
+> = Fn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8, Param9], ReturnType>
+export type AsyncFn9<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    Param9 = any,
+    ReturnType = void
+> = AsyncFn<[Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8, Param9], ReturnType>
+export type Fn10<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    Param9 = any,
+    Param10 = any,
+    ReturnType = void
+> = Fn<
+    [Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8, Param9, Param10],
+    ReturnType
+>
+export type AsyncFn10<
+    Param1 = any,
+    Param2 = any,
+    Param3 = any,
+    Param4 = any,
+    Param5 = any,
+    Param6 = any,
+    Param7 = any,
+    Param8 = any,
+    Param9 = any,
+    Param10 = any,
+    ReturnType = void
+> = AsyncFn<
+    [Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8, Param9, Param10],
+    ReturnType
+>
 
 /** A wrapper for the `Parameters<T>` type helper */
 export type Param<
